6a2ce7f86fab3b1f6478553aaf8d3f6563e289ba
davidcooper1 <davidpcooper1@gmail.com>
2020-04-27 20:48:36 -0500

    Fixed errors with copying files.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const chokidar = require('chokidar');
-const execFile = require('child_process').exec;
+const exec = require('child_process').exec;
+const path = require('path');
+
+process.chdir(path.dirname(__filename));
+process.env.ProjectFilePath = path.resolve(process.env.ProjectFilePath);
 
 if (!process.env.ProjectFilePath)
     throw "Project file path not specified.";
@@ -8,6 +12,13 @@ if (!process.env.ProjectFilePath)
 if (!process.env.ProjectName)
     throw "Project name not specified";
 
+console.log(`Watching ${process.env.ProjectFilePath}`)
+
 chokidar.watch(process.env.ProjectFilePath).on('change', () => {
-    execFile('./localdeploy.ps1');
+    exec("PowerShell -ExecutionPolicy Bypass -File .\\localdeploy.ps1", function(err, stdout, stderr) {
+        if (err)
+            throw err;
+        console.log(stdout);
+        console.log(stderr);
+    });
 });
